b6ea40a987809797a7df6ebb8a960bb52cdfe96d
서정민 <suhjm5009@dencomm.ai>
2023-04-03 16:23:12 +0900

    Blog Layout & Setting

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='py-3 text-center bg-slate-900'>
+      <h2 className='text-lg font-semibold text-slate-300'>{"Don't forget to CODE ypur DREAM | All Right Reserved"}</h2>
+    </footer>
+  );
+}
