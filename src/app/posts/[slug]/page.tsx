de9ea21b198ee58af34857c00853cea61bd20093
서정민 <suhjm5009@dencomm.ai>
2023-04-06 16:17:53 +0900

    Post 상세글에 이전&다음 포스팅 이동버튼 하단에 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import AdjacentPostCard from '@/components/AdjacentPostCard';
 import PostContent from '@/components/PostContent';
 import { getPostData } from '@/service/post';
 import Image from 'next/image';
@@ -11,14 +12,16 @@ export default async function PostDetailPage({ params: { slug } }: Props) {
   // 1. 전달된 slug에 해당하는 포스트 데이터를 읽어와서
   // 2. 데이터를 마크다운뷰어에 표기하면 됨
   const post = await getPostData(slug);
-  const {path, title} = post
+  const { path, title, prev, next } = post;
 
   return (
-    <div>
-      <article className='rounded-t-2xl overflow-hidden'>
-        <Image className='w-full h-1/5 max-h-[500px]' src={`/images/posts/${path}.png`} alt={title} width={500} height={200}/>
-      </article>
+    <article className='rounded-t-2xl overflow-hidden'>
+      <Image className='w-full h-1/5 max-h-[500px]' src={`/images/posts/${path}.png`} alt={title} width={500} height={200} />
       <PostContent post={post} />
-    </div>
+      <section className='flex shadow-md'>
+        {prev && <AdjacentPostCard post={prev} type={`prev`} />}
+        {next && <AdjacentPostCard post={next} type={`next`} />}
+      </section>
+    </article>
   );
 }
