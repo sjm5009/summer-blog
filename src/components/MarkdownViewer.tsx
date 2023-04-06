70b65fc4d00f4b669905b0632e21d853dadcdddd
서정민 <suhjm5009@dencomm.ai>
2023-04-06 13:59:47 +0900

    Markdown에러 수정

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -9,31 +9,30 @@ import Image from 'next/image';
 export default function MarkdownViewer({ content }: { content: string }) {
   return <>
         <ReactMarkdown
-        className='prose max-w-none'
-        remarkPlugins={[remarkGfm]}
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
-              <SyntaxHighlighter children={String(children).replace(/\n$/, '')} language={match[1]} PreTag='div' {...props} style={coldarkDark} />
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          },
-          img: (image) => (
-            <Image
-            className='w-full max-h-64'
+          className='prose max-w-none'
+          children={content}
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter children={String(children).replace(/\n$/, '')} language={match[1]} PreTag='div' {...props} style={coldarkDark} />
+                ) : (
+                  <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+            img: (image) => (
+              <Image
+              className='w-full max-h-64'
               src={image.src || ''}
               alt={image.alt || ''}
               width={500}
               height={350}
-            />
-          ),
-        }}
-      >
-        {content}
-      </ReactMarkdown>
-  </>;
+              />
+              ),
+            }}
+      />
+  </>
 }
