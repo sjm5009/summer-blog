4a5a5ea3ae50132aa2f25f25af617b2250af0dfb
sjm5009 <57248107+sjm5009@users.noreply.github.com>
2023-04-06 18:32:45 +0900

    About 추가 (#6)
    * Profile 추가
    
    * Blog 1차 순한맛 버전 만들기
    
    * 빌드 실패 에러 수정
    
    * 빌드 실패 에러 수정 ver2
    
    * Post 상세글 markdown하기 & react-icons 추가 & TailwindCss Typography 추가
    
    * Markdown에러 수정
    
    * Markdown에러 수정 Ver2
    
    * Markdown에러 수정 Ver3
    
    * Markdown에러 수정 Ver4
    
    * Markdown에러 수정 Ver5
    
    * Post 상세글에 이전&다음 포스팅 이동버튼 하단에 추가
    
    * Delete package-lock.json
    
    * 타입수정
    
    * 배포 오류 수정
    
    * About
    
    ---------
    
    Co-authored-by: 서정민 <suhjm5009@dencomm.ai>

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,30 @@
+import Hero from '@/components/Hero';
+
+const DIV_CLASS = 'mb-3';
+const H1_CLASS = 'font-bold text-2xl';
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <>
+      <Hero />
+      <section className='bg-pink-50 p-10 text-center mt-[30px] rounded-3xl'>
+        <div className={DIV_CLASS}>
+          <h1 className={H1_CLASS}>Who Am I?</h1>
+          <p>업무시간에만 개발에 흥미를 느끼는 풀스택 개발자</p>
+          <p>그래도 공부할 땐 재밌게 열심히 합니다.</p>
+        </div>
+        <div className={DIV_CLASS}>
+          <h1 className={H1_CLASS}>Career</h1>
+          <p>Dencomm (~ Now)</p>
+          <p>ITkey (~ 2018)</p>
+        </div>
+        <div className={DIV_CLASS}>
+          <h1 className={H1_CLASS}>Skills</h1>
+          <p>React, Node</p>
+          <p>Git</p>
+          <p>VS Code, Eclipse, IntelliJ</p>
+          <p>Oracle, Mysql, Firebase, MongoDB</p>
+        </div>
+      </section>
+    </>
+  );
 }
