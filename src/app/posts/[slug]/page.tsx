a207b8259fe241457995adc97b2013825b9560ae
서정민 <suhjm5009@dencomm.ai>
2023-04-06 13:47:14 +0900

    Post 상세글 markdown하기 & react-icons 추가 & TailwindCss Typography 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,6 @@
+import PostContent from '@/components/PostContent';
 import { getPostData } from '@/service/post';
+import Image from 'next/image';
 
 type Props = {
   params: {
@@ -9,11 +11,14 @@ export default async function PostDetailPage({ params: { slug } }: Props) {
   // 1. 전달된 slug에 해당하는 포스트 데이터를 읽어와서
   // 2. 데이터를 마크다운뷰어에 표기하면 됨
   const post = await getPostData(slug);
+  const {path, title} = post
 
   return (
     <div>
-      <h1>{post.title}</h1>
-      <pre>{post.content}</pre>
+      <article className='rounded-t-2xl overflow-hidden'>
+        <Image className='w-full h-1/5 max-h-[500px]' src={`/images/posts/${path}.png`} alt={title} width={500} height={200}/>
+      </article>
+      <PostContent post={post} />
     </div>
   );
 }
