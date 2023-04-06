bf2d4611e6426bf86d152e3f240062b19c01363f
서정민 <suhjm5009@dencomm.ai>
2023-04-06 14:47:48 +0900

    Markdown에러 수정 Ver3

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+'use client';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
@@ -7,32 +6,29 @@ import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Image from 'next/image';
 
 export default function MarkdownViewer({ content }: { content: string }) {
-  return <>
-        <ReactMarkdown
-          className='prose max-w-none'
-          children={content}
-          remarkPlugins={[remarkGfm]}
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || '');
-              return !inline && match ? (
-                <SyntaxHighlighter children={String(children).replace(/\n$/, '')} language={match[1]} PreTag='div' {...props} style={coldarkDark} />
-                ) : (
-                  <code className={className} {...props}>
-                  {children}
-                </code>
-              );
-            },
-            img: (image) => (
-              <Image
-              className='w-full max-h-64'
-              src={image.src || ''}
-              alt={image.alt || ''}
-              width={500}
-              height={350}
-              />
-              ),
-            }}
-      />
-  </>
+  return (
+    <section>
+      <ReactMarkdown
+        className='prose max-w-none'
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter language={match[1]} PreTag='div' {...props} style={coldarkDark}>
+                children={String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+          img: (image) => <Image className='w-full max-h-64' src={image.src || ''} alt={image.alt || ''} width={500} height={350} />,
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+    </section>
+  );
 }
